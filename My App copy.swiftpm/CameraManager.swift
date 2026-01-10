73d0882f919d9ddb7fb0f6cfb053284d7a3531db
Micael Martins de Moura <micael.mdemoura@gmail.com>
2026-01-09 21:42:40 -0300

    feat: adicionado o rastreamento de dedo indicador e polegar.

--- My App copy.swiftpm/CameraManager.swift
+++ My App copy.swiftpm/CameraManager.swift
@@ -1,100 +1,112 @@
 import AVFoundation
 import Vision
 
-final class CameraManager: NSObject {
+// Essa classe configura a camera, pode iniciar ou parar e entrega os frames para o app.
+final class CameraManager: NSObject, ObservableObject {   // É uma classe do tipo final por que não pode ser herdada por outras classes.
 
-    let session = AVCaptureSession()
-    private let videoQueue = DispatchQueue(label: "camera.video.queue")
-
-    private let handPoseRequest: VNDetectHumanHandPoseRequest = {
+    @Published var thumbPoint: CGPoint? = nil
+    @Published var indexPoint: CGPoint? = nil
+    
+    let session = AVCaptureSession() // instancia uma AVCaptureSession dentro de session. orquestrando os inputs e outputs
+    private let videoQueue = DispatchQueue(label: "camera.video.queue") // Cria uma instância constante videoQueue. Define em qual thread os frames da camera serão entregues
+   
+    private let handPoseRequest: VNDetectHumanHandPoseRequest = { // Essa constante, que é do tipo VNDetectHumanHandPoseRequest, cria um request que é também do mesmo tipo, configura o máximo de mãos detectadas para um e retorna request para HandPoseRequest. Não entendo o por que desse código ser escrito dessa maneira.
         let request = VNDetectHumanHandPoseRequest()
-        request.maximumHandCount = 1
-        return request
-    }()
+            request.maximumHandCount = 1
+            return request
+        }()
+    
+    
 
-    func start() {
+    func start() { // Essa função é a responsável por iniciar a camera e também as demais funções presentes neste arquivo. Ele configura as configurações de camera feitas abaixo e diz "vai"
         print("Starting camera")
         configureSession()
         session.startRunning()
     }
 
-    private func configureSession() {
-        session.sessionPreset = .high
+    private func configureSession() { // Essa função tem como principal objetivo a configuração da camera que será usada.
+        session.sessionPreset = .high // É a configuração da qualidade
 
-        guard
-            let camera = AVCaptureDevice.default(
-                .builtInWideAngleCamera,
-                for: .video,
-                position: .front
-            ),
-            let input = try? AVCaptureDeviceInput(device: camera)
+        guard let camera = AVCaptureDevice.default( //Observa se a camera será aberta, se for ela ficará no molde de vídeo, com a camera frontal e eu não sei o que é o primeiro.
+            .builtInWideAngleCamera,
+            for: .video,
+            position: .front
+        ),
+        let input = try? AVCaptureDeviceInput(device: camera) // Vai tentar receber como input a camera, se não conseguir, vai retornar nada a função.
         else { return }
 
-        if session.canAddInput(input) {
+        if session.canAddInput(input) { // Se for possível adicionar input, então adicione input? Não entendi isso também.
             session.addInput(input)
         }
 
-        let output = AVCaptureVideoDataOutput()
-        output.videoSettings = [
+        let output = AVCaptureVideoDataOutput() // Acredito que esta parte do código seja responsável por fazer o recebimento dos frames que são usados pelo vision para identificar a presença de uma mão ou não.
+        output.videoSettings = [ // Configura como o output vai ser processado.
             kCVPixelBufferPixelFormatTypeKey as String:
                 kCVPixelFormatType_32BGRA
         ]
 
-        output.setSampleBufferDelegate(self, queue: videoQueue)
+        output.setSampleBufferDelegate(self, queue: videoQueue) // Faço nem ideia
 
-        if session.canAddOutput(output) {
+        if session.canAddOutput(output) { // Se for possível ter o output, então adicione esse output a sessão?
             session.addOutput(output)
         }
     }
 }
 
-extension CameraManager: AVCaptureVideoDataOutputSampleBufferDelegate {
-
+extension CameraManager: AVCaptureVideoDataOutputSampleBufferDelegate { // Eu entendo como extensões funcionam, mas não entendi a necessidade de fazer isso aqui.
+    
     func captureOutput(
         _ output: AVCaptureOutput,
         didOutput sampleBuffer: CMSampleBuffer,
         from connection: AVCaptureConnection
     ) {
-        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
+        guard let pixelBuffer = // Não sei o que isso faz
+                CMSampleBufferGetImageBuffer(sampleBuffer)
         else { return }
-
-        let handler = VNImageRequestHandler(
+        
+        let handler = VNImageRequestHandler(  // Configuração do Vision para ele entender o que ele vai analisar, como e as opções?
             cvPixelBuffer: pixelBuffer,
-            orientation: .up,
+            orientation: .right,
             options: [:]
         )
-
+        
         do {
-            try handler.perform([handPoseRequest])
-
-            guard
-                let observation = handPoseRequest.results?.first
-            else { return }
-
-            let thumbPoints = try observation.recognizedPoints(.thumb)
-            let indexPoints = try observation.recognizedPoints(.indexFinger)
-
-            guard
-                let thumbTip = thumbPoints[.thumbTip],
-                let indexTip = indexPoints[.indexTip],
-                thumbTip.confidence > 0.6,
-                indexTip.confidence > 0.6
-            else { return }
-
-            let dx = thumbTip.location.x - indexTip.location.x
-            let dy = thumbTip.location.y - indexTip.location.y
-            let distance = sqrt(dx * dx + dy * dy)
-
-            let pinchThreshold: CGFloat = 0.04
-
-            if distance < pinchThreshold {
-                print("🤏 Pinça FECHADA")
-            } else {
-                print("✋ Pinça ABERTA")
+            try handler.perform([handPoseRequest]) // O vision tentará executar o handler VNImageRequestHandler com o handPoseRequest
+            
+            guard let observation = handPoseRequest.results?.first else { return } // Em observation será guardado o primeiro resultado, se houver algum, de hadPoseRequest. Se for nulo, retornará.
+            
+                let thumbPoints = try observation.recognizedPoints(.thumb) // Aqui ele vai pegar o dicionário de pontos do dedão
+                let indexPoints = try observation.recognizedPoints(.indexFinger) // Aqui ele vai pegar o dicionário de pontos do dedo indicador
+                
+                guard
+                    
+                    let thumbTip = thumbPoints[.thumbTip], // Aqui ele vai garantir que thumbTip e indexTip tenham ambos um nível de confiança acima de 0.6, e que ambos existam. Se não ele retornará.
+                    let indexTip = indexPoints[.indexTip],
+                    
+                        thumbTip.confidence > 0.6,
+                    indexTip.confidence > 0.6
+                else { return }
+            
+            DispatchQueue.main.async {
+                self.thumbPoint = thumbTip.location
+                self.indexPoint = indexTip.location
             }
-
-        } catch {
-            print("Erro Vision:", error)
+                
+                let dx = thumbTip.location.x - indexTip.location.x // Aqui é feita a conta que define se os dois pontos estão próximos ou não. Porém, eu não entendi a conta.
+                let dy = thumbTip.location.y - indexTip.location.y
+                let distance = sqrt(dx * dx + dy * dy)
+                
+                let pinchThreshold: CGFloat = 0.04 // Define a distância máxima para uma pinça
+                
+                if distance < pinchThreshold { // Se a distança entre os pontos do dedo indicador e dedão forem menor que 0.04 a pinça está fechada
+                    print("🤏 Pinça FECHADA")
+                } else { // se não, está aberta.
+                    print("✋ Pinça ABERTA")
+                }
+                
+            } catch { // Se o vision não conseguir ser executado, ele vem pra cá, sinalizando o que o impediu de funcionar e analisar a mão.
+                print("Erro Vision: ", error)
+            }
         }
     }
-}
+
--- My App copy.swiftpm/ContentView.swift
+++ My App copy.swiftpm/ContentView.swift
@@ -2,12 +2,36 @@ import SwiftUI
 
 struct ContentView: View {
 
-    private let cameraManager = CameraManager()
+    @StateObject private var cameraManager = CameraManager()
 
     var body: some View {
-        CameraPreviewView(session: cameraManager.session)
-            .onAppear {
+        GeometryReader { geo in
+            
+            ZStack {
+              Color.black.ignoresSafeArea()
+                // area visivel de cada tela deverá ser colocada aqui
+                
+                if let thumb = cameraManager.thumbPoint {
+                    Circle()
+                        .fill(Color.white)
+                        .frame(width: 20, height: 20)
+                        .position(
+                            x: (1 - thumb.x) * geo.size.width,
+                            y: (1 - thumb.y) * geo.size.height
+                        )
+                }
+                if let index = cameraManager.indexPoint {
+                    Circle()
+                        .fill(Color.white)
+                        .frame(width: 20, height: 20)
+                        .position(
+                            x: (1 - index.x) * geo.size.width,
+                            y: ( 1 - index.y) * geo.size.height
+                        )
+                }
+            }.onAppear {
                 cameraManager.start()
             }
+        }
     }
 }
