eb889a189a12dbd9a13d51058ec88e0abd8620b9
Micael Martins de Moura <micael.mdemoura@gmail.com>
2026-01-22 11:35:39 -0300

    refactor: refiz a definção de cores, retirando a necessidade do arquivo de conversão hex para decimal

--- My App copy.swiftpm/ContentView.swift
+++ My App copy.swiftpm/ContentView.swift
@@ -13,37 +13,39 @@ struct ContentView: View {
     }
 
     var body: some View {
-        GeometryReader { geo in
-            ZStack {
-                
-                CameraPreviewView(session: cameraManager.session).ignoresSafeArea()
-//                Color.black.ignoresSafeArea()
-
-                // Polegar
-                if let thumb = cameraManager.thumbPoint {
-                    Circle()
-                        .fill(cameraManager.isNext ? Color.red : Color.white)
-                        .frame(width: 20, height: 20)
-                        .position(
-                            x: (1 - expand(thumb.x)) * geo.size.width,
-                            y: (1 - expand(thumb.y)) * geo.size.height
-                        )
-                }
-
-                // Indicador
-                if let index = cameraManager.indexPoint {
-                    Circle()
-                        .fill(cameraManager.isNext ? Color.red : Color.white)
-                        .frame(width: 20, height: 20)
-                        .position(
-                            x: ( 1 - expand(index.x)) * geo.size.width,
-                            y: (1 - expand(index.y)) * geo.size.height
-                        )
-                }
-            }
-            .onAppear {
-                cameraManager.start()
-            }
+        ZStack {
         }
+//        GeometryReader { geo in
+//            ZStack {
+//                
+//                CameraPreviewView(session: cameraManager.session).ignoresSafeArea()
+////                Color.black.ignoresSafeArea()
+//
+//                // Polegar
+//                if let thumb = cameraManager.thumbPoint {
+//                    Circle()
+//                        .fill(cameraManager.isNext ? Color.red : Color.white)
+//                        .frame(width: 20, height: 20)
+//                        .position(
+//                            x: (1 - expand(thumb.x)) * geo.size.width,
+//                            y: (1 - expand(thumb.y)) * geo.size.height
+//                        )
+//                }
+//
+//                // Indicador
+//                if let index = cameraManager.indexPoint {
+//                    Circle()
+//                        .fill(cameraManager.isNext ? Color.red : Color.white)
+//                        .frame(width: 20, height: 20)
+//                        .position(
+//                            x: ( 1 - expand(index.x)) * geo.size.width,
+//                            y: (1 - expand(index.y)) * geo.size.height
+//                        )
+//                }
+//            }
+//            .onAppear {
+//                cameraManager.start()
+//            }
+//        }
     }
 }
---
+++ My App copy.swiftpm/Views/SceneView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  My App
+//
+//  Created by Micael Martins de Moura on 21/01/26.
+//
+
+import SwiftUI
+
+struct SceneView: View {
+    
+    var body: some View {
+        ZStack {
+            Color(.sceneBackground).ignoresSafeArea()
+        }
+    }
+}
+
+#Preview {
+    SceneView()
+}
